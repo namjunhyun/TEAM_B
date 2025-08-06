6b3cf7c93d844f8320691db2010b881e57b7cdc1
jaebeommm <gsxxx007@naver.com>
2025-08-07 00:21:04 +0900

    CORS 수정

--- TEAM_B_backend/src/main/java/com/example/TEAM_B_backend/core/config/WebConfig.java
+++ TEAM_B_backend/src/main/java/com/example/TEAM_B_backend/core/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 엔드포인트에 대해
-        .allowedOrigins("https://saymary-frontend.vercel.app") // Vercel 우리 프론트 주소
+                .allowedOriginPatterns("https://*.vercel.app") // Vercel 우리 프론트 주소에서 *로 수정
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true); // 필요한 경우 인증정보 허용
