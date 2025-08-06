6fb93d5f972f4a7d8ea728030876156fdd9641a7
jaebeommm <gsxxx007@naver.com>
2025-08-06 17:22:29 +0900

    컨트롤러 에러 처리 추가

--- TEAM_B_backend/src/main/java/com/example/TEAM_B_backend/archive/controller/getAllFilesController.java
+++ TEAM_B_backend/src/main/java/com/example/TEAM_B_backend/archive/controller/getAllFilesController.java
@@ -4,10 +4,13 @@ import com.example.TEAM_B_backend.archive.dto.FileDetailResponseDto;
 import com.example.TEAM_B_backend.archive.dto.TextFileDto;
 import com.example.TEAM_B_backend.archive.fileService.TextFileService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,13 +21,39 @@ public class getAllFilesController {
 
     // 파일 목록 조회: userId 기반
     @GetMapping
-    public ResponseEntity<List<TextFileDto>> getFileList(@RequestParam Long userId) {
-        return ResponseEntity.ok(textFileService.getFileList(userId));
+    public ResponseEntity<?> getFileList(@RequestParam Long userId) {
+        try {
+            List<TextFileDto> fileList = textFileService.getFileList(userId);
+            return  ResponseEntity.ok(fileList);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(Map.of("error", "해당 사용자의 파일 목록을 찾을 수 없습니다"));
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(Map.of("error", "서버 오류가 발생했습니다."));
+        }
     }
 
     // 파일 상세 조회: fileId 기반 (원문, 요약 세가지, 코칭은 제외!)
     @GetMapping("/{fileId}/details")
-    public ResponseEntity<FileDetailResponseDto> getFileDetails(@PathVariable Long fileId) {
-        return ResponseEntity.ok(textFileService.getFileDetails(fileId));
+    public ResponseEntity<?> getFileDetails(@PathVariable Long fileId) {
+        try {
+            FileDetailResponseDto details = textFileService.getFileDetails(fileId);
+            return ResponseEntity.ok(details);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(Map.of("error", "해당 사용자의 파일 목록을 찾을 수 없습니다"));
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(Map.of("error", "서버 오류가 발생했습니다."));
+        }
     }
 }
