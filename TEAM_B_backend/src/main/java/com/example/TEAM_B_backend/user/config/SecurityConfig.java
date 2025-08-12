676764568da72d5310c9dd7ed2ab0fb827b8f6ae
jaebeommm <gsxxx007@naver.com>
2025-08-13 03:34:42 +0900

    config 수정 3차

--- TEAM_B_backend/src/main/java/com/example/TEAM_B_backend/user/config/SecurityConfig.java
+++ TEAM_B_backend/src/main/java/com/example/TEAM_B_backend/user/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable()) // 버전에 따라 방식 변경
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // 프리플라이트 전부 허용
-                        .requestMatchers("/api/user/login","/api/user/signup").permitAll() // 로그인/인증 공개
+                        .requestMatchers(HttpMethod.POST,"/api/user/login","/api/user/signup").permitAll() // 로그인/인증 공개
                         .anyRequest().authenticated() // 모든 요청 허용 -> authenticated() 인증필요
                 );
 //                .formLogin(form -> form
@@ -38,9 +38,9 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cfg = new CorsConfiguration();
         cfg.setAllowCredentials(true); //쿠키/자격증명 포함 허용
+        cfg.setAllowedOrigins(List.of("https://saymary.site"));
         cfg.setAllowedOriginPatterns(List.of(
-                "https://*.vercel.app",
-                "https://saymary.site"
+                "https://*.vercel.app"
         ));
         cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         cfg.setAllowedHeaders(List.of("*"));
