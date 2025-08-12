a9dd50a0bb07635c7e0958e195174a50b86bc624
jaebeommm <gsxxx007@naver.com>
2025-08-13 02:57:21 +0900

    스프링 config 수정2

--- TEAM_B_backend/src/main/java/com/example/TEAM_B_backend/user/config/SecurityConfig.java
+++ TEAM_B_backend/src/main/java/com/example/TEAM_B_backend/user/config/SecurityConfig.java
@@ -24,13 +24,13 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable()) // 버전에 따라 방식 변경
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // 프리플라이트 전부 허용
-                        .requestMatchers("/api/user/login", "/api/auth/**").permitAll() // 로그인/인증 공개
-                        .anyRequest().authenticated() // 모든 요청 허용 -> authenticated()
-                )
-                .formLogin(form -> form
-                    .loginProcessingUrl("/login") // 로그인 요청을 처리할 URL
-                        .permitAll()
+                        .requestMatchers("/api/user/login","api/user/signup").permitAll() // 로그인/인증 공개
+                        .anyRequest().authenticated() // 모든 요청 허용 -> authenticated() 인증필요
                 );
+//                .formLogin(form -> form
+//                    .loginProcessingUrl("/login") // 로그인 요청을 처리할 URL
+//                        .permitAll()
+//                );
         return http.build();
     }
 
