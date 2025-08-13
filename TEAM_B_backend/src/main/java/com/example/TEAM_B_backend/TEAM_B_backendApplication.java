1e4748d67fb209d2b6c1bd649db92ea0f33134cd
jaebeommm <gsxxx007@naver.com>
2025-08-14 01:17:33 +0900

    import 추가

--- TEAM_B_backend/src/main/java/com/example/TEAM_B_backend/TEAM_B_backendApplication.java
+++ TEAM_B_backend/src/main/java/com/example/TEAM_B_backend/TEAM_B_backendApplication.java
@@ -2,8 +2,10 @@ package com.example.TEAM_B_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages = "com.example.TEAM_B_backend")
+@Import(SecurityConfig.class)
 public class TEAM_B_backendApplication {
 
     public static void main(String[] args) {
